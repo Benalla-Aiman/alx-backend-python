0bdda0ad7034aff8468de8805ba9ec9d0b7eb508
Benalla Aiman <benallaaiman@gmail.com>
2024-02-18 17:09:18 +0100

    correction

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
-""" Module documentation """
-from typing import List
+'''Task 3's module.
+'''
+import asyncio
 
-async_generator = __import__("0-async_generator").async_generator
+
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def async_comprehension() -> List[float]:
-    """Func doc"""
-    return [random async for random in async_generator()]
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    '''Creates an asynchronous task for wait_random.
+    '''
+    return asyncio.create_task(wait_random(max_delay))
